676aad9b08ff5241720a204b16afb37867f15b7e
SonQ <soniaquintiao@gmail.com>
2019-06-13 01:54:27 +0100

    header, footer, body Updates
    Header finalised.
    
    Main body scrolls behind header + img updaged to cows.
    
    Sticky footer wasn't looking good - normal footer  added with social icon links + other footer links.

--- js/myscript.js
+++ js/myscript.js
@@ -1,7 +1,7 @@
 // How To, Typewriter, w3 schools
 var i = 0;
-var txt = "Fire away! We'll do our best to help. Or, send us your suggestions and we'll be in touch to add your content in due course."; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
+var txt = "HOTCoW would love to hear from you! Fire away by filling in the below contact form."; /* The text */
+var speed = 35; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
   if (i < txt.length) {
